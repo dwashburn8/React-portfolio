5137fd34f101eac514e79a19fd7494b7554f79a5
Dallas Washburn <soldbydw@gmail.com>
2020-07-30 20:30:06 -0700

    Updates

---
+++ precache-manifest.d5429eb3fcf44d31159b5eac0997df5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a7edee2e7d0ac419ed5c140e0dd7fb",
+    "url": "/React-portfolio/index.html"
+  },
+  {
+    "revision": "9869bc33e55c53d5f5d6",
+    "url": "/React-portfolio/static/css/main.e3d13ed9.chunk.css"
+  },
+  {
+    "revision": "746bef788416a66c2b11",
+    "url": "/React-portfolio/static/js/2.b71896c1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-portfolio/static/js/2.b71896c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9869bc33e55c53d5f5d6",
+    "url": "/React-portfolio/static/js/main.c134b83b.chunk.js"
+  },
+  {
+    "revision": "a93d179556bca06183fc",
+    "url": "/React-portfolio/static/js/runtime-main.b79e20e6.js"
+  }
+]);
